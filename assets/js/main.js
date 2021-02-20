5bc2f8d3742511fb8742dfe981d659d31a9c7514
atiqbeenrahim <abr.royal@gmail.com>
2021-02-20 19:46:39 +0000

    Added google cluster map and fix JS, reveiw sec.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//Navbar toggle//
 $(document).ready(function(){
 
   $('.fa-bars').click(function(){
@@ -17,10 +18,10 @@ $(document).ready(function(){
     }
 
     $('section').each(function(){
-      var id = $(this).attr('id');
-      var height = $(this).height();
-      var offset = $(this).offset().top - 200;
-      var top = $(window).scrollTop();
+      let id = $(this).attr('id');
+      let height = $(this).height();
+      let offset = $(this).offset().top - 200;
+      let top = $(window).scrollTop();
       if(top >= offset && top < offset + height){
         $('.navbar ul li a').removeClass('active');
         $('.navbar').find('[data-scroll="' + id + '"]').addClass('active');
@@ -31,3 +32,30 @@ $(document).ready(function(){
 
 
 });
+
+//Google Map Clustar marker//
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: { lat: 54.07366927871658, lng: -6.280508196282103 },
+  });
+
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  const markers = locations.map((location, i) => {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+  new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
+const locations = [
+  { lat: 53.28758963736527, lng: -6.365464299036272 },
+  { lat: 53.34503530812306, lng: -6.267437318146202 },
+  { lat: 54.59766859658021, lng: -5.930882680741953 },
+];
