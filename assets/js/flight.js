60f76732641a36db629822b916b27ae0b0a03609
atiqbeenrahim <abr.royal@gmail.com>
2021-03-01 15:48:26 +0000

    change flight js code

--- assets/js/flight.js
+++ assets/js/flight.js
@@ -1,33 +1,37 @@
-async function makeAPICall(url, headers) {
-	const response = await fetch(url, {
-		headers,
-	    });
-	return response.json();
-}
+
 async function onSubmit() {
-	var destination = document.getElementById("destination").value;
-	var dep = document.getElementById("dep-date").value;
-	var rtn =  document.getElementById("rtn-date").value;
-	var response = await makeAPICall(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/IE/EUR/en/DUB-sky/${destination}-sky/${dep}?inboundpartialdate=${rtn}`,
-				 {
-					"method": "GET",
-					"headers": {
-						"x-rapidapi-key": "1fdda8b8d7msh0ff4bfb2ac7e81ep1bfce7jsnd180ee116950",
-						"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-				       }
+	var dest = document.getElementById("destination").value;
+    await fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/IE/EUR/en-IE/DUB-sky/${dest}-sky/anytime?inboundpartialdate=anytimes`, {
+		methor: 'GET',
+		headers: {
+			"x-rapidapi-key": "1fdda8b8d7msh0ff4bfb2ac7e81ep1bfce7jsnd180ee116950",
+			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
 				}
-			);
-			return response;
-
+		})
+		.then(response => {
+			console.log(response);
+			if (!response.ok) {
+				throw Error("ERROR")
+			}
+			return response.json();
+		})
+		.then(data => {
+			console.log(data.Quotes);
+			const html = data.Quotes.map(flight => {
+				return `
+				<div class="flight">
+				<p>${flight.Carriers}</p>
+				<p>${flight.Currencies}</p>
+				<p>${flight.Direct}</p>
+				</div>`
+			})
+			.join("");
+			document
+			.querySelector('#s-result')
+			.insertAdjacentHTML("afterbegin", html);
+		})
+		.catch(error => {
+			console.log(error);
+		});
 }
-
-var result = [];
-		var d = [];
-		d = response.json;
-		for (var i in d) {
-			d[i].id = i;
-			result.push(d[i]);
-		}
-		localStorage.setItem("fligtdata", JSON.stringify(result));
-		//window.open('./flight.html', '_blank');
-		window.location = './flight.html';
+onSubmit();
